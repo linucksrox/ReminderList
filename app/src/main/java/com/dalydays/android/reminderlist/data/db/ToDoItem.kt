f06078f4b479311a26fc0b49635bcd682662b93d
linucksrox <ericdalyblog@dalydays.com>
2019-07-02 07:19:45 -0400

    Add duration and timeUnit to the model, change fab icon to standard add plus icon

--- app/src/main/java/com/dalydays/android/reminderlist/data/db/ToDoItem.kt
+++ app/src/main/java/com/dalydays/android/reminderlist/data/db/ToDoItem.kt
@@ -3,6 +3,7 @@ package com.dalydays.android.reminderlist.data.db
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.concurrent.TimeUnit
 
 @Entity(tableName = "item")
 data class ToDoItem(
@@ -10,5 +11,7 @@ data class ToDoItem(
         @ColumnInfo(name = "sort") var sort: Long? = null,
         @ColumnInfo(name = "description") var description: String,
         @ColumnInfo(name = "completed") var completed: Boolean = false,
-        @ColumnInfo(name = "scheduled") var scheduled: Boolean = false
+        @ColumnInfo(name = "scheduled") var scheduled: Boolean = false,
+        @ColumnInfo(name = "duration") var duration: Int? = null,
+        @ColumnInfo(name = "time_unit") var timeUnit: TimeUnit? = null
 )
