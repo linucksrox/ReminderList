760cdc768bb753ddf7bcf91f62e505a0bef25fc4
linucksrox <ericdalyblog@dalydays.com>
2019-07-29 13:20:45 -0400

    Remove unused imports

--- app/src/main/java/com/dalydays/android/reminderlist/ui/checklist/ChecklistFragment.kt
+++ app/src/main/java/com/dalydays/android/reminderlist/ui/checklist/ChecklistFragment.kt
@@ -1,13 +1,10 @@
 package com.dalydays.android.reminderlist.ui.checklist
 
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
