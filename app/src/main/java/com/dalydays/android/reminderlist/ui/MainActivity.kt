30d791b2283db6b018db9d5b7e8984335c427dcf
linucksrox <ericdalyblog@dalydays.com>
2019-08-28 10:22:22 -0400

    Integrate navigate up/back into toolbar for all non top level screens

--- app/src/main/java/com/dalydays/android/reminderlist/ui/MainActivity.kt
+++ app/src/main/java/com/dalydays/android/reminderlist/ui/MainActivity.kt
@@ -2,14 +2,29 @@ package com.dalydays.android.reminderlist.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.dalydays.android.reminderlist.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(toolbar)
+
+        navController = Navigation.findNavController(this, R.id.my_nav_host_fragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        // From the documentation: "If you do not have a DrawerLayout, you should call NavController.navigateUp() directly."
+        return navController.navigateUp()
     }
 }
