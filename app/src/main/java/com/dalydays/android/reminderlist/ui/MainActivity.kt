0dcfea7c574b84f07e4849c9e1bfcf14aa33bf16
Eric Daly <edaly@hpsnet.com>
2021-07-29 14:07:01 -0400

    Use Java 11 instead of Java 8

--- app/src/main/java/com/dalydays/android/reminderlist/ui/MainActivity.kt
+++ app/src/main/java/com/dalydays/android/reminderlist/ui/MainActivity.kt
@@ -3,7 +3,6 @@ package com.dalydays.android.reminderlist.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.dalydays.android.reminderlist.R
